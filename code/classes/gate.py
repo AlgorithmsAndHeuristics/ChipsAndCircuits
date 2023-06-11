a5c91074f2f5795b9db1d14d6f8d825289f24543
Feyenoord Rotterdam <feyeneuuuurd@gmail.com>
2023-06-11 15:51:15 +0200

    quick style fix

--- code/classes/gate.py
+++ code/classes/gate.py
@@ -7,7 +7,6 @@ class Gate():
         PRE: Id of type integer, position of type tuple[int, int]
         POST: Initializes a Gate object"""
 
-
         self.id = id
         self.position = position
 
@@ -15,10 +14,11 @@ class Gate():
     def get_distance(self, other_gate: 'Gate') -> int:
         """
         Gets the distance between two gates.
-        PRE: other_gate of type Gate
-        POST: distance between the gates rounded up as int"""
+        PRE: Other_gate of type Gate
+        POST: Distance between the gates rounded up as int"""
 
 
         dx = abs(self.position[0] - other_gate.position[0])
         dy = abs(self.position[1] - other_gate.position[1])
+
         return int(math.sqrt(dx**2 * dy**2))
