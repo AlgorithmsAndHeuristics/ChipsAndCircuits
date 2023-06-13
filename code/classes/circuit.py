1a191901759ddec521457fddff96f870eab5c0b6
Joey Blankendaal <JoeyBlankendaal@users.noreply.github.com>
2023-06-13 23:36:36 +0200

    Fix grid representation when containing gate IDs longer than one digit

--- code/classes/circuit.py
+++ code/classes/circuit.py
@@ -11,37 +11,23 @@ class Circuit():
         self.gates: list[Gate] = []
         
         print_x = pd.read_csv(print_path)
+        self.cell_width: int = 1
         
         # Read the gates from the print_x file
         for index, gate in print_x.iterrows():
             self.gates.append(Gate(int(gate[0]), (int(gate[1]), int(gate[2]))))
+            
+            # Determine cell width by getting the gate coordinate with the longest length
+            gate_x_len: int = len(str(gate[1]))
+            gate_y_len: int = len(str(gate[2]))
+            
+            if gate_x_len > self.cell_width:
+                self.cell_width = gate_x_len
+            
+            if gate_y_len > self.cell_width:
+                self.cell_width = gate_y_len
         
         self.make_grid(factor)
-    
-
-    def make_grid(self, factor: int) -> None:
-        """
-        Make the grid with a multiplying factor.
-        PRE: Factor of type int, must be larger or equal to 1
-        POST: Grid is dimensions are multiplied by factor,
-        center stays the same"""
-
-        assert factor >= 1, "Can't make grid smaller than biggest distance"
-
-        # Create the grid based on the furthest gates
-        org_width = max([gate.position[0] for gate in self.gates])
-        org_height = max([gate.position[1] for gate in self.gates])
-        grid_width = int(org_width * factor)
-        grid_height = int(org_height * factor)
-        self.grid: list[list[str]] = [['_' for y in range(grid_height + 1)] for x in range(grid_width + 1)]
-        
-        # Place the gates on the grid
-        x_move = (grid_width - org_width) // 2
-        y_move = (grid_height - org_height) // 2
-
-        for gate in self.gates:
-            gate.position = (gate.position[0] + x_move, gate.position[1] + y_move)
-            self.grid[gate.position[0] - 1][gate.position[1] - 1] = gate.id
 
     
     def __repr__(self) -> str:  
@@ -68,7 +54,59 @@ class Circuit():
 
         return board_str
     
+
+    def get_gate_id(self, gate: Gate) -> str:
+        gate_id: str = str(gate.id)
+        
+        # For each digit that a gate ID is shorter than cell width, append a whitespace character
+        if self.cell_width > len(gate_id):
+            for _ in range(self.cell_width - len(gate_id)):
+                gate_id += ' '
+        
+        return gate_id
+    
+    
+    def get_underscore(self) -> str:
+        underscore: str = '_'
+        
+        if self.cell_width > 1:
+            for _ in range(self.cell_width - 2):
+                underscore += '_'
+            
+            underscore += ' '
+        
+        return underscore
+    
     
     def load_netlist(self, path: str) -> None:
         self.netlists.append(Netlist(path))
+    
+    
+    def make_grid(self, factor: int) -> None:
+        """
+        Make the grid with a multiplying factor.
+        PRE: Factor of type int, must be larger or equal to 1
+        POST: Grid is dimensions are multiplied by factor,
+        center stays the same"""
 
+        assert factor >= 1, "Can't make grid smaller than biggest distance"
+
+        # Set characters based on cell width
+        underscore: str = self.get_underscore()
+        
+        # Create the grid based on the furthest gates
+        org_width = max([gate.position[0] for gate in self.gates])
+        org_height = max([gate.position[1] for gate in self.gates])
+        
+        grid_width = int(org_width * factor)
+        grid_height = int(org_height * factor)
+        
+        self.grid: list[list[str]] = [[underscore for y in range(grid_height + 1)] for x in range(grid_width + 1)]
+        
+        # Place the gates on the grid
+        x_move = (grid_width - org_width) // 2
+        y_move = (grid_height - org_height) // 2
+
+        for gate in self.gates:
+            gate.position = (gate.position[0] + x_move, gate.position[1] + y_move)
+            self.grid[gate.position[0] - 1][gate.position[1] - 1] = self.get_gate_id(gate)
--- main.py
+++ main.py
@@ -4,43 +4,44 @@ sys.path.append(os.path.join(directory, "code"))
 sys.path.append(os.path.join(directory, "code", "classes"))
 sys.path.append(os.path.join(directory, "code", "algorithms"))
 
-# Import used datastructures
+# Import used data structures
 from circuit import Circuit
 from gate import Gate
 from netlist import Netlist
 from net import Net
 
-if __name__ == "__main__":
-    #circ = Circuit("data/chip_0/print_0.csv")
-    circ = Circuit("data/chip_1/print_1.csv")
 
-    print(f"Initial circuit:\n{circ}")
+chip: int = 1
+netlist: int = 4
 
-    circ.load_netlist("data/chip_0/netlist_1.csv")
 
-    print(f"Netlist 1 has {len(circ.netlists[0].nets)} nets")
+def print_nets(circuit: Circuit):
+    print(f"Netlist {netlist} has {len(circ.netlists[0].nets)} nets")
     print(f"A connection is required from gates:")
 
     for net in circ.netlists[0].nets:
         print(f"Gate {net.gates[0]} to gate {net.gates[1]}")
 
-    for wire in [(5,2), (4,2), (3,2), (3,3), (2,3)]:
-        circ.netlists[0].nets[0].add_wire(wire[0], wire[1])
 
-    print(f"\nCircuit after adding a wire from gate 1 to gate 5:\n{circ}")
+if __name__ == "__main__":
+    # Set circuit
+    circ = Circuit(f"data/chip_{chip}/print_{chip}.csv")
+    print(f"Initial circuit:\n{circ}")
 
-    print(f"Current cost: {circ.netlists[0].get_cost()}")
+    circ.load_netlist(f"data/chip_{chip}/netlist_{netlist}.csv")
+    #print_nets(circ)
 
-    for wire in [(5,5), (5,4), (5,3), (5,2)]:
-        circ.netlists[0].nets[1].add_wire(wire[0], wire[1])
+    #for wire in [(5,2), (4,2), (3,2), (3,3), (2,3)]:
+    #    circ.netlists[0].nets[0].add_wire(wire[0], wire[1])
 
-    print(f"\nCircuit after adding a wire from gate 2 to gate 1:\n{circ}")
+    #print(f"\nCircuit after adding a wire from gate 1 to gate 5:\n{circ}")
+    #print(f"Current cost: {circ.netlists[0].get_cost()}")
 
-    print(f"Current cost: {circ.netlists[0].get_cost()}")
+    #for wire in [(5,5), (5,4), (5,3), (5,2)]:
+    #    circ.netlists[0].nets[1].add_wire(wire[0], wire[1])
 
-    print("-----------------------")
-
-    print("Adding an enlargement factor increases the grid size")
-
-    print(Circuit("data/chip_0/print_0.csv", 2))
-    
+    #print(f"\nCircuit after adding a wire from gate 2 to gate 1:\n{circ}")
+    #print(f"Current cost: {circ.netlists[0].get_cost()}")
+    #print("-----------------------")
+    #print("Adding an enlargement factor increases the grid size")
+    #print(Circuit("data/chip_0/print_0.csv", 2))
